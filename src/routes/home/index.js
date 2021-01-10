f77545a1c44e405121e887f5ae7f63cad2f0b714
Zack Cerza <zack@redhat.com>
2021-01-10 16:50:28 -0700

    fix unnecessary render

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -10,9 +10,10 @@ import style from './style.css';
 const Home = () => {
   const [date, setDate] = useState(new Date());
   const [editing, setEditing] = useState(false);
+  const formatted_date = format(date, "yyyy-MM-dd");
   return (
     <div class={style.home}>
-      <h1 class={style.big_date}>{ format(date, "yyyy-MM-dd") }</h1>
+      <h1 class={style.big_date}>{ formatted_date }</h1>
       <DatePicker date={ date } setDate={ setDate } />
       <ActionList date={date} editing={editing} setEditing={setEditing} />
     </div>
