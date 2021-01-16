1bb0bc2ae6bbc46cb16a50b4a52e01fae9933c4e
Zack Cerza <zack@redhat.com>
2021-01-15 19:50:14 -0700

    replace date picker

--- src/components/date_picker/index.js
+++ src/components/date_picker/index.js
@@ -1,26 +1,26 @@
 import { useState } from 'preact/hooks';
-
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+import parse from 'date-fns/parse';
+import TextField from '@material-ui/core/TextField';
 
 import style from './style.css';
 
 function DatePicker(props) {
   const [value, _onChange] = useState(props.date || new Date());
-  const onChange = (date) => {
+  const onChange = (evt) => {
+    const date = parse(evt.target.value, 'yyyy-MM-dd', new Date());
     _onChange(date);
     props.setDate(date);
   }
-
   return (
-    <div>
-      <Calendar
+    <form noValidate>
+      <TextField
+        id="date"
+        type="date"
+        defaultValue={value}
         onChange={onChange}
-        value={value}
-        className={style.datepicker}
       />
-    </div>
-  );
+    </form>
+  )
 }
 
 export default DatePicker;
