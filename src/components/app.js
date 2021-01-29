761a4d93f6736b850f289e2127037fede2e92c68
Zack Cerza <zack@redhat.com>
2021-01-29 00:58:39 -0700

    auto dark mode

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,9 @@
 import { Router } from 'preact-router';
+import { useMemo } from 'preact/hooks';
+
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import Header from './header';
 
@@ -9,17 +14,30 @@ import Home from '../routes/home';
 import Profile from '../routes/profile';
 
 const App = () => {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const theme = useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
   return (
-    <div id="app">
-      <Header />
-      <ActionsProvider>
-      <Router>
-        <Home path="/" />
-        <Profile path="/profile/" user="me" />
-        <Profile path="/profile/:user" />
-      </Router>
-      </ActionsProvider>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div id="app">
+        <Header />
+        <ActionsProvider>
+        <Router>
+          <Home path="/" />
+          <Profile path="/profile/" user="me" />
+          <Profile path="/profile/:user" />
+        </Router>
+        </ActionsProvider>
+      </div>
+    </ThemeProvider>
   )
 }
 
